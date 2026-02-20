dbf0504b1e292887d34b98a032dc221f63d04566
alejandrodomin <a283le.programmer@gmail.com>
2026-02-20 13:17:28 -0500

    cahnged things to pure cv2, having trouble with the bounding boxes not encompassing human percieved objects

--- src/main.py
+++ src/main.py
@@ -3,28 +3,44 @@ import logging
 
 import cv2
 
-from bounding import bounding_boxes
-from filter import mov_filter
-
 logger = logging.getLogger(__name__)
+
+
+def input_parser():
+    parser = argparse.ArgumentParser(
+        description="Simple movement based edge detection. Use the <Q> key to escape window."
+    )
+    # Add arguments
+    parser.add_argument(
+        "--live",
+        help="Pulls from a webcam if enabled.",
+        default=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--input_file",
+        help="Path to mp4 file to transform.",
+        default="./data/video/fisherman.mp4",
+    )
+    parser.add_argument(
+        "--frame_rate", help="Frames per second.", default=20, metavar="[1-1000]"
+    )
+    parser.add_argument(
+        "--filter",
+        help="""Filters out white noise from the image. The higher the value the less gets filtered out.
+                        Everything is filtered at 0, nothing is filtered at 255.""",
+        default=100,
+        metavar="[0-255]",
+    )
+
+    return parser
+
 
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    parser = argparse.ArgumentParser(
-        description="Simple movement based edge detection. Use the <Q> key to escape window.")
-    # Add arguments
-    parser.add_argument("--live", help="Pulls from a webcam if enabled.",
-                        default=False, action='store_true')
-    parser.add_argument("--input_file", help="Path to mp4 file to transform.",
-                        default="./data/video/balloons.mp4")
-    parser.add_argument("--frame_rate", help="Frames per second.", default=20, metavar="[1-1000]")
-    parser.add_argument("--filter",
-                        help="""Filters out white noise from the image. The higher the value the less gets filtered out.
-                         Everything is filtered at 0, nothing is filtered at 255.""",
-                        default=235, metavar="[0-255]")
     # Parse arguments
-    args = parser.parse_args()
+    args = input_parser().parse_args()
     # Access arguments
     live = args.live
     file = args.input_file
@@ -38,27 +54,40 @@ def main():
     else:
         capture = cv2.VideoCapture(0)
 
+    # Create window for boxed frame
+    window_name = "4. boxed"
+    cv2.namedWindow(window_name)
+    cv2.createTrackbar("Noise Filter", window_name, fltr, 255, lambda x: None)
+
     last_frame = None
     while capture.isOpened():
         ret, frame = capture.read()
         if not ret:
             break
 
-        cv2.namedWindow('Boxed', cv2.WINDOW_NORMAL)
-        cv2.namedWindow('filter', cv2.WINDOW_NORMAL)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow("1. gray", gray)
 
         if last_frame is not None:
-            transformed_frame = mov_filter(gray, last_frame, fltr)
+            # Get current filter value from trackbar
+            fltr = cv2.getTrackbarPos("Noise Filter", window_name)
+
+            transformed_frame = cv2.absdiff(gray, last_frame)
+            # cv2.imshow("2. transformed", transformed_frame)
+
+            thresh = cv2.threshold(transformed_frame, fltr, 255, cv2.THRESH_BINARY)[1]
+            # cv2.imshow("3. thresh", thresh)
 
             boxed_frame = frame.copy()
-            for (top_left, bot_rght) in bounding_boxes(transformed_frame):
-                cv2.rectangle(boxed_frame, top_left, bot_rght, (0, 255, 0), 2)
+            cnts = cv2.findContours(
+                thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS
+            )[0]
+            for c in cnts:
+                (x, y, w, h) = cv2.boundingRect(c)
+                cv2.rectangle(boxed_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.imshow(window_name, boxed_frame)
 
-            cv2.imshow('filter', transformed_frame)
-            cv2.imshow('Boxed', boxed_frame)
-
-            if cv2.waitKey(1000 // fr) & 0xFF == ord('q'):
+            if cv2.waitKey(1000 // fr) & 0xFF == ord("q"):
                 break
 
         last_frame = gray
@@ -66,5 +95,6 @@ def main():
     capture.release()
     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
